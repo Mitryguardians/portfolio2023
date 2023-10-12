d1fb6500fc8872e33727cee98d375503d01d778d
Mitryguardians <90128686+Mitryguardians@users.noreply.github.com>
2023-10-12 11:08:30 +0200

    Add files via upload

---
+++ content/js/index.js
@@ -0,0 +1,29 @@
+const modal = document.getElementById('modal')
+
+function modalimg(site){
+
+    const image1 = document.getElementById('imgOne')
+    const image2 = document.getElementById('imgTwo')
+    const image3 = document.getElementById('imgThree')
+    const image4 = document.getElementById('imgFour')
+    const image5 = document.getElementById('imgFive')
+    const leave = document.getElementById('leave')
+    modal.style.display = "block"
+    if(site === 1){
+        image1.src = "../img/sitelycee/cpt1.png"
+        image2.src = "../img/sitelycee/cpt2.png"
+        image3.src = "../img/sitelycee/cpt3.png"
+        image4.style.display = "none"
+        image5.style.display = "none"
+    }
+    if(site === 2){
+        image1.src = "../img/sitestreaming/cpt1.jpg"
+        image2.src = "../img/sitestreaming/cpt2.jpg"
+        image3.src = "../img/sitestreaming/cpt3.jpg"
+        image4.style.display = "none"
+        image5.style.display = "none"
+    }
+    leave.addEventListener('click', ()=>{
+        modal.style.display = "none"
+    })
+}
